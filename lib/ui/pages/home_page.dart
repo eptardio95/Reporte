debc9962dcdaab3d53a39f42a809ef30f6d7ecee
Eduardo <eptardio@gmail.com>
2022-08-29 10:52:03 -0400

    limpiando main.dart

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -9,11 +9,6 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 
 import 'form_usuario_page.dart';
 
-List<Widget> pages = [
-  ListaContactosPage(),
-  InformacionUsuarioPage(),
-  NoticiasPage(),
-];
 
 class HomePage extends StatefulWidget {
   @override
@@ -22,7 +17,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String titulo = "AppBar";
-  int picker = 0;
 
   Future<String> descargaImagen() async {
     print("Se inicia la descarga de la imagen");
@@ -42,79 +36,20 @@ class _HomePageState extends State<HomePage> {
                 child: Container(
                   color: Theme.of(context).primaryColor,
                 )),
-            ListTile(
-              leading: Icon(MdiIcons.homeCircle),
-              title: Text('Inicio'),
-              onTap: () {
-                setState(() {
-                  picker = 0;
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            ListTile(
-              leading: Icon(MdiIcons.accountBox),
-              title: Text('Usuario'),
-              onTap: () {
-                setState(() {
-                  picker = 1;
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            ListTile(
-              leading: Icon(MdiIcons.newspaper),
-              title: Text('Noticias'),
-              onTap: () {
-                setState(() {
-                  picker = 2;
-                  Navigator.pop(context);
-                });
-              },
-            ),
           ]),
         ),
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
 
-          actions: <Widget>[
-            picker == 1
-                ? IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () async {
-                      Navigator.pushNamed(context, 'form_usuario');
-
-                      //   print("Se presiono el boton");
-                      //
-                      //   descargaImagen().then(
-                      //           (value) {
-                      //       print(value);
-                      //       print("Se esta mostrando la imagen");
-                      // });
-
-                      // await descargaImagen();
-                    },
-                  )
-                : SizedBox()
-          ],
 
           title: Text(titulo),
         ),
         floatingActionButton: Builder(
           builder: (BuildContext context) {
             return FloatingActionButton(
-              onPressed: () async {
-                _controller.changeTheme(false);
-                presionandoBoton();
-                var resultado = await obtenerNoticias();
-                if (resultado != null) {
-                  print(resultado);
-                } else {
-                  Scaffold.of(context).showSnackBar(
-                      SnackBar(content: Text('No hay conexion a internet'),duration: Duration(seconds: 1),));
-                }
-              },
+              onPressed: (){},
+
               tooltip: 'Increment',
               child: const Icon(Icons.add),
             );
@@ -122,6 +57,6 @@ class _HomePageState extends State<HomePage> {
         ),
 
         /// Se puede usar Column pero son muchos elementos y no caben en la pantalla
-        body: pages[picker]);
+        body: Container());
   }
 }
